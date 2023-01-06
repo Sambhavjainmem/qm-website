f56ce3f8d99ee2f3c88526abe1cec851b317ced1
sambhav <sambhavjainmem@gmail.com>
2023-01-06 16:50:56 +0530

    invoice data fetch

--- src/components/cartVue.vue
+++ src/components/cartVue.vue
@@ -699,6 +699,7 @@ export default {
         id: docRef.id,
         mechanicId: "NA",
         mechanicName: "NA",
+        state: this.$store.state.currentState,
         paid: this.radioGroup == "Self" ? false : true,
         serviceId: pickupId,
         serviceType: "Scheduled",
@@ -964,6 +965,7 @@ export default {
 };
 </script>
  <style scoped>
+
 .v-stepper__step__step {
   background-color: #d50;
   color: red;
@@ -972,6 +974,7 @@ export default {
 .v-sheet.v-stepper {
   height: calc(100vh - 67px);
 }
+
 .carttitle {
   font-size: 18px;
   font-weight: 400;
@@ -984,6 +987,7 @@ export default {
   justify-content: space-between;
   align-items: center;
 }
+
 .v-list-item {
   padding: 0px;
 }
